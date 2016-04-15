2c0ecc7da233145fdd10d4f9fba28a45a2202295
Alexander Maksimovich <mak4alex@gmail.com>
2016-04-15 23:39:52 +0300

    Add cart state persistance

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -6,7 +6,8 @@ import { routerMiddleware } from 'react-router-redux';
 import { persistState } from 'redux-devtools';
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
-
+import immutablejs from 'redux-storage-decorator-immutablejs';
+import * as storage from 'redux-storage';
 
 const logger = createLogger({
   level: 'info',
@@ -15,15 +16,27 @@ const logger = createLogger({
 
 const router = routerMiddleware(browserHistory);
 
+const reducer = storage.reducer(rootReducer);
+
+import createEngine from 'redux-storage-engine-localstorage';
+let engine = createEngine('unicorn-shop');
+engine = immutablejs(engine, [
+  ['cart'],
+]);
+
+
+const storeMiddleware = storage.createMiddleware(engine);
+
+
 /**
  * Creates a preconfigured store.
  */
 export default function configureStore(initialState) {
   const store = createStore(
-    rootReducer,
+    reducer,
     initialState,
     compose(
-      applyMiddleware(thunk, router, logger),
+      applyMiddleware(thunk, storeMiddleware, router, logger),
       DevTools.instrument(),
       persistState(
         window.location.href.match(
@@ -40,6 +53,16 @@ export default function configureStore(initialState) {
       store.replaceReducer(nextRootReducer);
     });
   }
+
+  const load = storage.createLoader(engine);
+  load(store);
+
+  // Notice that our load function will return a promise that can also be used
+  // to respond to the restore event.
+  load(store)
+      .then((newState) => console.log('Loaded state:', newState))
+      .catch(() => console.log('Failed to load previous state'));
+
 
   return store;
 }
