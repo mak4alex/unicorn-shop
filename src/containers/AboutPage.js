136391853e647e5fcf07a89bfb95a775eec14257
promo1996 <bristik@mail.ru>
2016-04-30 15:34:49 +0000

    fix about page

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -5,7 +5,15 @@ export default class AboutPage extends Component {
   render() {
     return (
       <div>
-        <h1>AboutPage</h1>
+        <div className="page-header">
+        <h1 >AboutPage</h1>
+        </div>
+        <div className="jumbotron">
+          <h1>Hello, world!</h1>
+          <p>...</p>
+          <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
+        <div className="main">Text</div>
       </div>
     );
   }
