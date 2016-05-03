d5abda73e676b3084a106e550146672c246153e7
Alexander Maksimovich <mak4alex@gmail.com>
2016-05-03 11:10:22 +0300

    Improve cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -21,48 +21,60 @@ export default class Cart extends Component {
   render() {
     const lineItems = this.props.cart.get('lineItems');
     const total = this.props.cart.get('total');
+    let template = null;
 
-    return (
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          <h3 className="panel-title text-center">Shopping Cart{' '}
-            <span className="label label-default">{total}$</span>        
-          </h3>
-        </div>
-        <div className="panel-body">          
-          {
-            lineItems.map((lineItem, i) => {
-              const product = lineItem.get('product');
 
-              return (
-                <div key={i} className="row">
-                  <div className="input-group">
-                    <span className="input-group-addon">{product.get('title')}</span>
-                    <input type="number" className="form-control" min="1" step="1" value={lineItem.get('count')}
-                      onChange={ (e) => this.changeCount(e, product.get('id')) } 
-                    />
-                    <span className="input-group-btn">
-                      <button className="btn btn-default" onClick={(e) => this.removeProductFromCart(lineItem)}>
-                        <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                      </button>
-                    </span>
+    if (lineItems.size !== 0) {
+      template = (
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title text-center">Shopping Cart{' '}
+              <span className="label label-default">{total}$</span>
+            </h3>
+          </div>
+          <div className="panel-body">
+            {
+              lineItems.map((lineItem, i) => {
+                const product = lineItem.get('product');
+
+                return (
+                  <div key={i} className="row">
+                    <div className="input-group">
+                      <span className="input-group-addon">{product.get('title')}</span>
+                      <input type="number" className="form-control" min="1" step="1" value={lineItem.get('count')}
+                        onChange={ (e) => this.changeCount(e, product.get('id')) } 
+                      />
+                      <span className="input-group-btn">
+                        <button className="btn btn-default" onClick={(e) => this.removeProductFromCart(lineItem)}>
+                          <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                        </button>
+                      </span>
+                    </div>
                   </div>
-                </div>
-              );
-            })
-          }
-        </div>
-        <div className="panel-footer">
-          <div className="btn-group btn-group-justified" role="group">
-            <div className="btn-group" role="group">
-              <Link to={'/checkout'} className="btn btn-default">Checkout</Link>
-            </div>
-            <div className="btn-group" role="group">
-              <button className="btn btn-default" onClick={this.props.clearCart}>Empty</button>
+                );
+              })
+            }
+          </div>
+          <div className="panel-footer">
+            <div className="btn-group btn-group-justified" role="group">
+              <div className="btn-group" role="group">
+                <Link to={'/checkout'} className="btn btn-default">Checkout</Link>
+              </div>
+              <div className="btn-group" role="group">
+                <button className="btn btn-default" onClick={this.props.clearCart}>Empty</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      template = (
+        <div className="well well-sm text-center">
+          <h4>You cart is empty :(</h4>
+        </div>
+      );
+    }
+
+    return template;
   }
 }
